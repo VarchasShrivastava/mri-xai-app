7f40c38ea8d5da044f1c49ffb81ac371c4d64ab5
VarchasShrivastava <168369199+VarchasShrivastava@users.noreply.github.com>
2025-12-15 03:03:22 +0530

    Add files via upload

--- model.py
+++ model.py
@@ -1,11 +1,20 @@
 import torch
 import torch.nn as nn
 from torchvision import models
+import gdown
+import os
+
+MODEL_URL = "https://drive.google.com/uc?id=14S66AvfW1F4Q6nY2BRvJ1Lr6yPGlO5Y5"
+MODEL_PATH = "mri_xai_model.pth"
 
 def load_model(model_path, device):
+    # Download model if not present
+    if not os.path.exists(MODEL_PATH):
+        gdown.download(MODEL_URL, MODEL_PATH, quiet=False)
+
     model = models.resnet18(pretrained=False)
     model.fc = nn.Linear(model.fc.in_features, 2)
-    model.load_state_dict(torch.load(model_path, map_location=device))
+    model.load_state_dict(torch.load(MODEL_PATH, map_location=device))
     model.to(device)
     model.eval()
     return model
